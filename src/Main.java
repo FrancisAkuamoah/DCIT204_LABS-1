1369de85d182751e0c335196876b8667f96b94d4
FRANCIS KWAME AKUAMOAH <fkakuamoah001@st.ug.edu.gh>
2026-06-15 18:29:00 +0000

    Implement array input and search algorithms
    Added user input for array size and elements, and implemented linear and binary search algorithms with timing.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
-public class Main {
+ import java.util.Scanner;
+ public class Main {
     public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+        System.out.print("Enter array size: ");
+        int x = scr.nextInt();
+        int[] arr = new int [x];
+        int arraySize = x;
+        System.out.println("Array size: " + arraySize);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Enter element " + i + ": ");
+            arr[i] = scr.nextInt();
+
+        }    
+       
+        int result = Algorithms.LinearSearch(arr, 2);
+
+        int result = Algorithms.BinarySearch(arr, 2);
+
+        long startTime = System.nanoTime();
+
+        long endTime = System.nanoTime();
+
+        long duration = endTime -startTime;
+
+        System.out.println("Duration" + duration + "nanoseconds");
+
+        if(result != -1) {
+            System.out.println("Element found at key " + result);
+        }
+        else {
+            System.out.println("NOT FOUND");
+        }
+
+
 
     }
+
 }
