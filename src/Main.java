54fc6d55654f03c3a45ae391f0541b930c8e7fcc
FRANCIS KWAME AKUAMOAH <fkakuamoah001@st.ug.edu.gh>
2026-06-16 09:31:10 +0000

    "Implemented Linear Search and Binary Search"
    Added user input for array size and elements, and implemented linear and binary search algorithms.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
-public class Main {
+ import java.util.Scanner;
+ public class Main {
     public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+        System.out.print("Enter array size: ");
+        int x = scr.nextInt();
+        int[] arr = new int [x];
+        int arraySize = x;
+        System.out.println("Array size: " + arraySize);
 
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Enter element " + i + ": ");
+            arr[i] = scr.nextInt();
+
+        }    
+       
+       
+        long startTime = System.nanoTime();
+
+        long endTime = System.nanoTime();
+
+        long duration = endTime -startTime;
+
+        System.out.println("Duration" + duration + "nanoseconds");
+
+
+
+        int result = Algorithms.LinearSearch(arr, 2);
+        if(result != -1) {
+            System.out.println(result);
+            System.out.println("Element found at index: " + result);
+        }
+        else {
+            System.out.println("NOT FOUND");
+        }
+
+
+        
+        int result2 = Algorithms.BinarySearch(arr, 2);
+        if(result2 != -1) {
+             System.out.println(result2);
+            System.out.println("Element found at index: " + result2);
+        }
+        else {
+            System.out.println("NOT FOUND");
+        }
     }
 }
